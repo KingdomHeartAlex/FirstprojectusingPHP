a98a94915c967e052e4b0b9fd1bdb87b4b7fe1a5
Alexandra <alexandra.ad.formation@gmail.com>
2022-02-05 17:15:35 +0100

    First board created, test on board coded on another boarded, work on boucle while

---
+++ Site de recettes de cuisine/premierTableauUtilisateurs.php
@@ -0,0 +1,23 @@
+<?php
+
+// Premier utilisateur 
+$userName1 = 'Mickaël Andrieu';
+$userEmail1 = 'mickael.andrieu@exemple.com';
+$userPassword1 = 'S3cr3t';
+$userAge1 = 34;
+
+// Deuxième Utilisatrice
+$UserName2 = 'Laurène Castor';
+$UserEmail2 ='laurene.castor@exemple.com';
+$userPassword2 = 'P4ssW0rD';
+$userAge2 = 28;
+
+
+$user1 = ['Mickaël Andrieu', 'email', 'S3cr3t', 34];
+
+echo $user1[0]; // "Mickaël Andrieu"
+echo $user1[1]; // "email"
+echo $user1[3]; // 34
+
+
+?>
---
+++ jeNeDoisPasRegarderLesMouchesVolerQuandJapprendsLePhp.php
@@ -0,0 +1,10 @@
+<?php
+$lines = 1;
+
+while ($lines<=100) {
+    echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP,<br
+    />';
+        $lines++; // $lines = $lines + 1
+}
+
+?>
