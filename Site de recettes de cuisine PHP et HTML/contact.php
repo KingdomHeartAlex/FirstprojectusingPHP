d7413d04f8e88c7def5114154895131785885d8e
Alexandra <alexandra.ad.formation@gmail.com>
2022-02-08 12:51:25 +0100

    Travail sur Administrez des formulaires de façon sécurisée + la possibilité d'inclure l'envoi de fichier avec la superfonction $_FILES

--- Site de recettes de cuisine PHP et HTML/contact.php
+++ Site de recettes de cuisine PHP et HTML/contact.php
@@ -1,4 +1,6 @@
-<form action="submit_contact.php" method="GET">
+<form action="submit_contact.php" method="POST" enctype="multipart/form-data">
+    <!-- champs de formulaire -->
+</form>
     <div>
         <label for="email">Email</label>
         <input type="email" name="email">
@@ -7,5 +9,15 @@
         <label for="message">Votre message</label>
         <textarea placeholder="Exprimez vous" name="message"></textarea>
     </div>
-    <button type="submit">Envoyer</button>
+
+    <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
+    <!-- Ajout des champs email et message -->
+    [...]
+    <!-- Ajout champ d'upload ! -->
+    <div class="mb-3">
+        <label for="screenshot" class="form-label">Votre capture d'écran</label>
+        <input type="file" class="form-control" id="screenshot" name="screenshot" />
+    </div>
+    <!-- Fin ajout du champ -->
+    <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
